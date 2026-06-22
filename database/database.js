c4187c9128dc7e19fcb9375e2a65096e0a2e0768
wifimdc-byte <wifimdc@gmail.com>
2026-06-22 09:39:13 -0300

    Adicionar banco inicial

--- database/database.js
+++ database/database.js
@@ -1,14 +1,24 @@
 import sqlite3 from "sqlite3";
 import path from "path";
 
-const caminhoBanco = path.resolve("database", "banco.db");
+const caminhoBanco = process.env.DB_PATH
+    ? process.env.DB_PATH
+    : path.resolve("database", "banco.db");
+
+console.log("📁 Banco:", caminhoBanco);
 
 const db = new sqlite3.Database(caminhoBanco, (err) => {
+
     if (err) {
+
         console.error("Erro ao abrir banco:", err.message);
+
     } else {
-        console.log("Banco SQLite conectado.");
+
+        console.log("✅ Banco SQLite conectado.");
+
     }
+
 });
 
 export default db;
